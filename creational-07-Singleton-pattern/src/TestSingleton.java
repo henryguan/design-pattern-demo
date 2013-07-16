4a6aec76942ebf3e3c8f72e9546623339b2874a4
Henry Guan <hguanca@gmail.com>
2013-07-15 21:59:11 -0400

    singleton design pattern

---
+++ creational-07-Singleton-pattern/src/TestSingleton.java
@@ -0,0 +1,13 @@
+public class TestSingleton {
+	public static void main(String args[]) {
+		Database database;
+
+		database = Database.getInstance("products");
+
+		System.out.println("This is the " + database.getName() + " databse.");
+
+		database = Database.getInstance("employees");
+
+		System.out.println("This is the " + database.getName() + " databse.");
+	}
+}
---
+++ creational-07-Singleton-pattern/src/TestSingletonThreaded.java
@@ -0,0 +1,32 @@
+public class TestSingletonThreaded implements Runnable
+{
+  Thread thread;
+
+  public static void main(String args[])
+  {
+    TestSingletonThreaded t = new TestSingletonThreaded();
+  }
+
+  public TestSingletonThreaded()
+  {
+    DatabaseThreaded database;
+
+    database = DatabaseThreaded.getInstance("products");
+
+    thread = new Thread(this, "second");
+    thread.start(); 
+
+    System.out.println("This is the " + 
+      database.getName() + " databse.");
+  }
+
+    public void run() 
+    {
+      DatabaseThreaded database;
+
+      database = DatabaseThreaded.getInstance("employees");
+      
+      System.out.println("This is the " + 
+        database.getName() + " databse.");
+    }
+}
