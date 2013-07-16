b7663f0d90d273301127f67ae4df1a813aa8ad37
Henry Guan <hguanca@gmail.com>
2013-07-15 22:19:02 -0400

    new file

--- creational-07-Singleton-pattern/src/TestSingleton.java
+++ creational-07-Singleton-pattern/src/TestSingleton.java
@@ -1,13 +1,9 @@
 public class TestSingleton {
 	public static void main(String args[]) {
 		Database database;
-
 		database = Database.getInstance("products");
-
 		System.out.println("This is the " + database.getName() + " databse.");
-
 		database = Database.getInstance("employees");
-
 		System.out.println("This is the " + database.getName() + " databse.");
 	}
 }
